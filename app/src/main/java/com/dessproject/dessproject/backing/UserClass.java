751ec1073162215ae2474b1fdb44fa73b759c1bf
Eric <ericcorrado@gmail.com>
2017-11-05 14:53:02 -0500

    create userClass

---
+++ app/src/main/java/com/dessproject/dessproject/backing/UserClass.java
@@ -0,0 +1,44 @@
+package com.dessproject.dessproject.backing;
+
+/**
+ * Created by Eric on 11/5/2017.
+ */
+
+public class UserClass {
+    String userName;
+    String usedId;
+    String firstName;
+    String lastName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsedId() {
+        return usedId;
+    }
+
+    public void setUsedId(String usedId) {
+        this.usedId = usedId;
+    }
+}

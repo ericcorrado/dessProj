de044b1355ff1c0c52abddbe7e24e13c834bc3fb
Eric <ericcorrado@gmail.com>
2017-12-03 14:15:52 -0500

    create tag, post, wall classes. create test UI

--- app/src/main/java/com/dessproject/dessproject/backing/WallActivity.java
+++ app/src/main/java/com/dessproject/dessproject/backing/WallActivity.java
@@ -1,13 +1,14 @@
-package com.dessproject.dessproject.view;
+package com.dessproject.dessproject.backing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.dessproject.dessproject.R;
 
 public class WallActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_wall);
+        setContentView(R.layout.activity_wall);
     }
 }
